7b7a0387819bc9caf79885faf0110f71b0afb36f
lerma-dev <alejandrolerma256@gmail.com>
2026-04-10 04:37:33 -0600

    redirects

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://server-omnihub.onrender.com/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'https://server-omnihub.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
